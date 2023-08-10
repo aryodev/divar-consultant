aae5d159cde1920fbb0089bd39c4c3275c75e19f
aryo dev <devacc5680@gmail.com>
2023-08-10 18:17:13 +0330

    add port 8000

--- home/views.py
+++ home/views.py
@@ -34,7 +34,7 @@ class HomeView(View):
 
     def get(self, request, *args, **kwargs):
         # return redirect('admin/')
-        command = ['curl', 'localhost:1000/unlimited-search/?stop=status']
+        command = ['curl', 'localhost:8000/unlimited-search/?stop=status']
         try:
             res = run(command, capture_output=True, text=True, timeout=0.2)
         except TimeoutExpired:
@@ -135,7 +135,7 @@ class SearchView(View):
 
 class EnterSearchCommand(View):
     def get(self, request, *args, **kwargs):
-        command = ['curl', 'localhost:1000/unlimited-search/']
+        command = ['curl', 'localhost:8000/unlimited-search/']
         try:
             res = run(command, timeout=0.2, capture_output=True, text=True)
         except TimeoutExpired:
@@ -153,7 +153,7 @@ class EnterSearchCommand(View):
 
 class EnterStopSearchCommand(View):
     def get(self, request, *args, **kwargs):
-        command = ['curl', 'localhost:1000/unlimited-search/?stop=true']
+        command = ['curl', 'localhost:8000/unlimited-search/?stop=true']
 
         res = run(command, capture_output=True, text=True)
         res = loads(res.stdout)
